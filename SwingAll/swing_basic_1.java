6710c203d9be460293d2c1a3fa82033bef3b4d63
Niv Hirpara <nivhirpara250@gmail.com>
2025-12-04 22:15:25 +0530

    Create swing_basic_1.java

---
+++ SwingAll/swing_basic_1.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+class swing_basic_1
+{
+	public static void main(String args[])
+	{
+		JFrame f=new JFrame();
+		
+		f.setSize(500,500);
+		f.setLayout(null);
+		f.setVisible(true);
+		
+	}
+}
